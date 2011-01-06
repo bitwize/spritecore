f0653236994ef9fb2d6dc6ed074b27f1397eb834
bitwize <bitwize@mojo-king-bee.(none)>
2011-01-06 05:54:27 -0500

    TestIODelegate nearly complete

--- TestIODelegate.m
+++ TestIODelegate.m
@@ -34,21 +34,43 @@
 			 title: (char *)t {
 	self = [super init];
 	unsigned int dep = 32;
+	unsigned int i = 1;
 	host = ha;
-	// TODO: fill these
 	back.cx = w;
 	back.cy = h;
-	back.bits = NULL;
+	back.depth = 32;
+	back.scan_length = w * 4;
+	back.endian = (*((char *)(&i)) == 1 ?
+		      SIMG_LITTLE_ENDIAN   :
+		      SIMG_BIG_ENDIAN);
+	back.bits = malloc((size_t)(w * h * 4));
+	back.img = NULL;
+
 	buf.cx = w;
 	buf.cy = h;
-	buf.bits = NULL;
+	buf.depth = 32;
+	buf.scan_length = w * 4;
+	buf.endian = (*((char *)(&i)) == 1 ?
+		      SIMG_LITTLE_ENDIAN   :
+		      SIMG_BIG_ENDIAN);
+	buf.bits = malloc((size_t)(w * h * 4));
+	buf.img = NULL;
+
+	eq = malloc(sizeof(SpriteEvent) * MAX_QUEUE_DEPTH);
+	if(eq == NULL)
+	{
+		fprintf(stderr, "couldn't allocate syn event queue\n");
+		exit(-1);
+	}
+	q_start = 0;
+	q_end = 0;
 	return self;
 
 }
 
 - (SpriteImage *)backImage {return &back;}
 - (SpriteImage *)bufImage {return &buf;}
-- (void)refreshScreen {SDL_Flip(IMAGE(buf.img));}
+- (void)refreshScreen { }
 - (int)loadPPMFile: (char *)fn toImage: (SpriteImage *)si {
 	int _cx,_cy;
 	unsigned char *ptr;
@@ -58,15 +80,14 @@
 	si->img = (void *)SDL_CreateRGBSurfaceFrom(ptr,_cx,_cy,32,_cx * 4,
 						   0xff0000,0x00ff00,0x0000ff,
 						   0x0);
-	si->cx = IMAGE(si->img)->w;
-	si->cy = IMAGE(si->img)->h;
-	si->depth = IMAGE(si->img)->format->BitsPerPixel;
-	si->scan_length = IMAGE(si->img)->pitch;
-	si->endian = (SDL_BYTEORDER == SDL_LIL_ENDIAN ? 
-		      SIMG_LITTLE_ENDIAN : 
-		      SIMG_BIG_ENDIAN);
+	si->cx = _cx;
+	si->cy = _cy;
+	si->depth = 32;
+	si->scan_length = _cx * 4;
+	si->endian = buf.endian;
 	si->bits = IMAGE(si->img)->pixels;
 	si->auto_free = 1;
+	si->img = NULL;
 	return 0;
 
 }
@@ -74,91 +95,28 @@
 	     toImage: (SpriteImage *)si {
 	int _cx,_cy;
 	unsigned char *ptr;
-
 	ReadPpmRgbFromMemoryConverted(ppm,sz,&_cx,&_cy,&ptr,32);
 	if(ptr == NULL) {return -1;}
-	si->img = (void *)SDL_CreateRGBSurfaceFrom(ptr,_cx,_cy,32,_cx * 4,
-						   0xff0000,0x00ff00,0x0000ff,
-						   0x0);
 	si->cx = _cx;
 	si->cy = _cy;
-	si->depth = 24;
-	si->scan_length = _cx * 3;
-	si->endian = (SDL_BYTEORDER == SDL_LIL_ENDIAN ? 
-		      SIMG_LITTLE_ENDIAN : 
-		      SIMG_LITTLE_ENDIAN);
-	si->bits = IMAGE(si->img)->pixels;
+	si->depth = 32;
+	si->scan_length = _cx * 4;
+	si->endian = buf.endian;
+	si->bits = ptr;
 	si->auto_free = 1;
+	si->img = NULL;
 	return 0;
 
 }
 
 -(void)destroyImage: (SpriteImage *)si {
-	SDL_FreeSurface(IMAGE(si->img));
+	free(si->bits);
 
 }
 
 -(id)dispatchEvents {
-	SDL_Event evt;
-	int code;
-	int ks;
-	int i;
 	SpriteEvent se;
-	while(SDL_PollEvent(&evt)) {
-		switch(evt.type) {
-		case SDL_KEYDOWN:
-		case SDL_KEYUP:
-			se.source_type = SPRITEEVENT_SOURCE_KEYBOARD;
-			se.source_selector = 0;
-			ks = (evt.key.keysym).sym;
-			if(ks >= 0x01 && ks <= 0x7e) {
-				se.code = ks;
-				if(ks <= 0x20) {
-					se.code |= 0xff00;
-				}
-			}
-			else {
-				for(i=0;SC_SpecialKeyMap[i] != 0;i += 2) {
-					if(ks == SC_SpecialKeyMap[i]) {
-						se.code = SC_SpecialKeyMap[i + 1];
-					}
-				}
-			}
-			if ((evt.type) == SDL_KEYUP) {
-				se.value = 0;
-			} else
-			{
-				se.value = 1;
-			}
-			[host handleEvent: &se];
-			break;
-		case SDL_MOUSEMOTION:
-			se.source_type = SPRITEEVENT_SOURCE_MOUSE |
-				SPRITEEVENT_SOURCE_VALUATOR;
-			se.source_selector = 0;
-			se.code = 0;
-			se.value = evt.motion.x;
-			[host handleEvent: &se];
-			se.code = 1;
-			se.value = evt.motion.y;
-			[host handleEvent: &se];
-			break;
-		case SDL_MOUSEBUTTONDOWN:
-			se.source_type = SPRITEEVENT_SOURCE_MOUSE;
-			se.source_selector = 0;
-			se.code = evt.button.button;
-			se.value = 1;
-			[host handleEvent: &se];
-			break;
-		case SDL_MOUSEBUTTONUP:
-			se.source_type = SPRITEEVENT_SOURCE_MOUSE;
-			se.source_selector = 0;
-			se.code = evt.button.button;
-			se.value = 0;
-			[host handleEvent: &se];
-			break;
-		}
-	}
+	[host handleEvent: &se];
 }
 
 -(void)lockBuf {
@@ -177,7 +135,21 @@
 -(void)sleepMillis: (unsigned int) ms {
 	syn_clock += ms;
 }
+
+-(void)sendEvent: (SpriteEvent *)evt
+{
+	if((q_end + 1) % MAX_QUEUE_DEPTH == q_start)
+	{
+		return;
+	}
+	eq[q_end] = *evt;
+	q_end++;
+	q_end %= MAX_QUEUE_DEPTH;
+}
+
 -free {
+	[self destroyImage: &back];
+	[self destroyImage: &buf];
 	return [super free];
 }
 @end
--- include/SpriteCore/TestIODelegate.h
+++ include/SpriteCore/TestIODelegate.h
@@ -2,12 +2,16 @@
 
 #import <SpriteCore/SpriteApp.h>
 
+#define MAX_QUEUE_DEPTH 256
+
 @interface TestIODelegate : Object <SpriteIODelegate> {
 	SpriteApp *host;
 	SpriteImage back;
 	SpriteImage buf;
 	unsigned int syn_clock;
 	SpriteEvent *eq;
+	unsigned int q_start;
+	unsigned int q_end;
 }
 
 - (TestIODelegate *)initForHost: (SpriteApp *)ha
